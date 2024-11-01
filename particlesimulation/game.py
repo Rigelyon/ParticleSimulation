52c252dd0efa97a19f6dce00a82b9d910d795b10
Rigelyon <pandudwi120@gmail.com>
2024-11-02 02:13:04 +0700

    refactor gui element, added framerate counter text and video frame placeholder text

--- particlesimulation/game.py
+++ particlesimulation/game.py
@@ -5,6 +5,7 @@ from random import uniform, randint
 import pygame
 
 from particlesimulation.particles.particle import Particle
+from particlesimulation.menu import Menu
 from particlesimulation.settings import *
 
 
@@ -17,21 +18,24 @@ class Game:
         self.is_running = True
         self.max_frame_rate = 60
         self.particle_groups = pygame.sprite.Group()
-        self.font = pygame.font.SysFont(None, 24)
 
-        self.screen = pygame.Rect(20, 20, SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.menu = Menu()
 
     def update(self):
         self.window.fill(BG_COLOR)
         self.dt = self.clock.tick(self.max_frame_rate) / 1000
 
-        # Draw screen border
-        pygame.draw.rect(self.window, BASE_COLOR, self.screen, width=2)
-
-        # Draw debug text
         particle_count = len(self.particle_groups)
-        self.draw_text(self.window, f'Particle count: {particle_count}', (20, SCREEN_HEIGHT + 20 + 15), self.font,
-                       BASE_COLOR)
+        self.menu.draw_menu(self.window)
+        self.menu.draw_text(self.window, f'Particle(s) count: {particle_count}', (20, SCREEN_HEIGHT + 20 + 15),
+                            'midleft',
+                            self.menu.font_normal, BASE_COLOR)
+        self.menu.draw_text(self.window, f'Frame: 0/0', (20, SCREEN_HEIGHT + 20 + 35),
+                            'midleft',
+                            self.menu.font_normal, BASE_COLOR)
+        self.menu.draw_text(self.window, f'Frame rate: {round(self.clock.get_fps(), 2)}',
+                            (SCREEN_WIDTH + 20, SCREEN_HEIGHT + 20 + 15), 'midright',
+                            self.menu.font_normal, BASE_COLOR)
 
         self.particle_groups.draw(self.window)
         self.particle_groups.update(self.dt)
@@ -46,15 +50,9 @@ class Game:
         pos = (randint(30, SCREEN_WIDTH + 15), randint(25, SCREEN_HEIGHT + 20))
         direction = pygame.math.Vector2(0, -1)
         speed = randint(50, 100)
-        size = randint(5, 20)
+        size = randint(5, 10)
         Particle(groups=self.particle_groups, pos=pos, color=BASE_COLOR, direction=direction,
                  speed=speed, size=size)
-
-    def draw_text(self, surface, text, pos, font, color):
-        text = self.font.render(text, 1, color)
-        text_rect = (text.get_rect())
-        text_rect.midleft = pos
-        surface.blit(text, text_rect)
 
     def handle_events(self):
         for event in pygame.event.get():
---
+++ particlesimulation/menu.py
@@ -0,0 +1,23 @@
+import pygame
+
+from particlesimulation.settings import *
+
+
+class Menu:
+    def __init__(self):
+        self.screen = pygame.Rect(20, 20, SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.font_normal = pygame.font.SysFont(None, 24)
+
+    def draw_text(self, surface, text, pos, justification: ('midleft', 'midright'), font, color):
+        text = font.render(text, 1, color)
+        text_rect = (text.get_rect())
+        match justification:
+            case 'midleft':
+                text_rect.midleft = pos
+            case 'midright':
+                text_rect.midright = pos
+        surface.blit(text, text_rect)
+
+    def draw_menu(self, surface):
+        # Draw screen border
+        pygame.draw.rect(surface, BASE_COLOR, self.screen, width=2)

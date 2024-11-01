fd02e526b3a488b4fd4f999ac890cc29ba50f977
Rigelyon <pandudwi120@gmail.com>
2024-11-01 18:08:15 +0700

    feat: added base particle class

--- particlesimulation/game.py
+++ particlesimulation/game.py
@@ -1,20 +1,47 @@
 import sys
+from random import uniform, randint
 
 import pygame
 
-from settings import *
+from particlesimulation.particles.particle import Particle
+from particlesimulation.settings import *
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption(GAME_TITLE)
         self.clock = pygame.time.Clock()
         self.is_running = True
+        self.max_frame_rate = 60
+        self.particle_groups = pygame.sprite.Group()
 
-    def update(self, dt):
-        pass
+    def update(self):
+        self.screen.fill(BG_COLOR)
+        self.dt = self.clock.tick(self.max_frame_rate) / 1000
+
+        self.particle_groups.draw(self.screen)
+        self.particle_groups.update(self.dt)
+
+        while len(self.particle_groups) > 5000:
+            self.particle_groups.sprites()[0].kill()
+
+        pygame.display.update()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.is_running = False
+                self.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed(3)[2]:
+                    for _ in range(1010):
+                        pos = pygame.mouse.get_pos()
+                        direction = pygame.math.Vector2(uniform(-1, 1), uniform(-1, 1)).normalize()
+                        speed = randint(10, 100)
+                        Particle(groups=self.particle_groups, pos=pos, color=BASE_COLOR, direction=direction,
+                                 speed=speed)
 
     def quit(self):
         pygame.quit()
@@ -22,7 +49,7 @@ class Game:
 
     def loop(self):
         while self.is_running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.is_running = False
-                    self.quit()
+            self.handle_events()
+            self.update()
+
+            print(f'Particle count: {len(self.particle_groups)}')
---
+++ particlesimulation/particles/particle.py
@@ -0,0 +1,56 @@
+import pygame.sprite
+
+from particlesimulation.settings import *
+
+
+class Particle(pygame.sprite.Sprite):
+    def __init__(self,
+                 groups: pygame.sprite.Group,
+                 pos: list[int],
+                 color: str,
+                 direction: pygame.math.Vector2,
+                 speed: int,
+                 size: int = 8,
+                 fade_speed: int = 120):
+        super().__init__(groups)
+        self.pos = pos
+        self.color = color
+        self.direction = direction
+        self.size = size
+        self.speed = speed
+        self.alpha = 255
+        self.fade_speed = fade_speed
+
+        self.create_surf()
+
+    def create_surf(self):
+        self.image = pygame.Surface((self.size, self.size)).convert_alpha()
+        self.image.set_colorkey(BG_COLOR)
+        pygame.draw.circle(surface=self.image, color=self.color, center=(self.size / 2, self.size / 2),
+                           radius=self.size / 2)
+        self.rect = self.image.get_rect(center=self.pos)
+
+    def move(self, dt):
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = self.pos
+
+        # print(f'{self.alpha} = {self.fade_speed} * {dt}')
+
+    def check_pos(self):
+        if (self.pos[0] < -100 or self.pos[0] > SCREEN_WIDTH + 100 or
+                self.pos[1] < -100 or self.pos[1] > SCREEN_HEIGHT + 100):
+            self.kill()
+
+    def check_alpha(self):
+        if self.alpha < 0:
+            self.kill()
+
+    def fade(self, dt):
+        self.alpha -= self.fade_speed * dt
+        self.image.set_alpha(self.alpha)
+
+    def update(self, dt):
+        self.move(dt)
+        self.check_pos()
+        self.fade(dt)
+        self.check_alpha()

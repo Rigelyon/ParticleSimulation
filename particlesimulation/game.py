243d9ff999e24dbd7d917bbb5f694a126b74381d
Rigelyon <pandudwi120@gmail.com>
2024-11-05 00:00:35 +0700

    feat: implement basic button and slider with no functionality

--- particlesimulation/game.py
+++ particlesimulation/game.py
@@ -25,14 +25,19 @@ class Game:
 
         particle_count = len(self.particles.groups)
         self.ui.draw_screen()
-        self.ui.draw_text(self.window, f'Particle(s) count: {particle_count}', (SCREEN_MARGIN, SCREENY_BOTTOM + SCREEN_MARGIN),
-                            'midleft',
+        self.ui.draw_text(self.window, f'Particle(s) count: {particle_count}',
+                          (SCREEN_MARGIN, SCREENY_BOTTOM + SCREEN_MARGIN),
+                          'midleft',
                           self.ui.font_normal, BASE_COLOR)
         self.ui.draw_text(self.window, f'Frame: 0/0', (SCREEN_MARGIN, SCREENY_BOTTOM + SCREEN_MARGIN + 22),
-                            'midleft',
+                          'midleft',
                           self.ui.font_normal, BASE_COLOR)
         self.ui.draw_text(self.window, f'Frame rate: {round(self.clock.get_fps(), 2)}',
                           (SCREENX_RIGHT, SCREENY_BOTTOM + SCREEN_MARGIN), 'midright',
+                          self.ui.font_normal, BASE_COLOR)
+        self.ui.draw_text(self.window, f'Amount: {self.ui.size_slider.get_current_value()}',
+                          (SCREENX_RIGHT + SCREEN_MARGIN, SCREENY_TOP + 20 + PARTICLE_BT_SIZE[1] * 2),
+                          'midleft',
                           self.ui.font_normal, BASE_COLOR)
 
         self.particles.groups.draw(self.ui.screen_surf)
@@ -42,7 +47,9 @@ class Game:
         self.particles.check_max_particles()
         self.particles.kill_off_screen()
 
-        self.ui.blit(self.window)
+        self.ui.update(self.dt)
+        self.ui.surface_blit(self.window)
+        self.ui.draw_ui(self.window)
 
         pygame.display.update()
 
@@ -51,8 +58,8 @@ class Game:
             if event.type == pygame.QUIT:
                 self.is_running = False
                 self.quit()
-            if pygame.mouse.get_pressed()[0]:
-                print(event.pos)
+
+            self.ui.process_events(event)
 
     def quit(self):
         pygame.quit()
@@ -62,4 +69,7 @@ class Game:
         while self.is_running:
             self.handle_events()
             self.update()
-            self.particles.spawn_particle({'normal' : 10})
+
+            print(self.ui.size_slider.get_current_value())
+
+            # self.particles.spawn_particle('snow', 1,1,1,1,1,1)

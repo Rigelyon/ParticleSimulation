34305900f2354bbbb8e08a4cdb505df786b31464
Rigelyon <pandudwi120@gmail.com>
2024-11-24 19:15:55 +0700

    fix: color picker initial color now use current color

--- particlesimulation/ui.py
+++ particlesimulation/ui.py
@@ -306,7 +306,7 @@ class UI:
         self.color_picker = UIColourPickerDialog(
             pygame.Rect((WINDOW_WIDTH / 4, WINDOW_HEIGHT / 4), (500, 300)),
             ui_manager,
-            initial_colour=pygame.Color(BASE_COLOR),
+            initial_colour=ParticleFlag.current_color,
             window_title="Choose Color",
         )
 
